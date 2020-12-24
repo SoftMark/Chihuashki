b2811ab5d3adb9dd016cbfa39289166f6fc53462
Vladkolo <VailorJob@gmail.com>
2020-12-25 00:58:52 +0200

    Шаблон v0.1

--- ChiHuashki/static/js/script.js
+++ ChiHuashki/static/js/script.js
@@ -1,4 +1,12 @@
 let butRise = document.getElementById("topChi");
+let butNav = document.getElementsByClassName("navigate")
+
+for (var i = 0; i < butNav.length; i++)
+{
+    butNav[i].onclick = function () {
+        console.log(this.href)
+    }
+}
 
 window.addEventListener("scroll", function() {
     var value = window.scrollY;
@@ -8,4 +16,5 @@ window.addEventListener("scroll", function() {
     } else if (value === 0){
         butRise.style.display = "none"
     }
-})
+});
+
