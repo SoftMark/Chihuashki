5885192dc3d1f9b1bfe4c3054ae945c81f44cfd1
Vladkolo <Vladkolo2014@gmail.com>
2021-01-09 18:16:20 +0200

    ser

--- ChiHuashki/ChiHuashki/settings.py
+++ ChiHuashki/ChiHuashki/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "LendingApp.apps.LendingappConfig",
+    'django_cleanup',  # should go after your apps
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/content/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = STATIC_DIR
+# STATIC_ROOT = STATIC_DIR
 STATICFILES_DIRS = (STATIC_DIR,)
 
 # Путь у папке
