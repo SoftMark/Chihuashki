e36f8ce8d63b7cace38d72f602fd3f06486df2bc
ZinganDovgaluk <SoftMark228@gmail.com>
2021-01-12 00:08:33 +0200

    Изображения в админке, надо доделать

--- ChiHuashki/LendingApp/admin.py
+++ ChiHuashki/LendingApp/admin.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django.contrib import admin
-from .models import Chihuahua, Photo
+from .models import Chihuahua, Photo, GalleryImage
 
 # admin.site.register(Chihuahua)
 
@@ -14,17 +14,50 @@ class PhotoForm(ModelForm):
             *self.MIN_RESOLUTION)
 
 
-class GalleryInline(admin.TabularInline):
+# Чишки
+class ChihGalleryInline(admin.TabularInline):
     fk_name = 'chihuahua'
     model = Photo
-    form = PhotoForm
     extra = 1  # количество пустых форм
     max_num = 5  # максимальное количество форм
     verbose_name = 'image'
 
 
-
 @admin.register(Chihuahua)
 class ChihuahuaAdmin(admin.ModelAdmin):
-    inlines = [GalleryInline, ]
-    list_display = ("name", "age", "teeth")
+    #form = PhotoForm
+    inlines = [ChihGalleryInline, ]
+
+    list_display = ("name", 'image', "age", "teeth")
+    readonly_fields = ["get_all_images"]
+
+    def image(self, obj):
+        images = list(obj.images.all())
+        if images:
+            return images[0].image_img()
+        else:
+            return Photo.image_no_photo()
+
+    image.short_description = 'Фото'
+    image.allow_tags = True
+
+    def get_all_images(self, obj):
+        images = list(obj.images.all())
+        collected = []
+        for img in images:
+            collected.append(img.image_img())
+        return collected
+
+
+
+
+# Галерея
+@admin.register(GalleryImage)
+class GalleryAdmin(admin.ModelAdmin):
+    form = PhotoForm
+    list_display = ("image_name", "link", "image_img")
+    readonly_fields = ('image_img',)
+
+
+
+
