caf2694c101edd6a159afc7fbf3bde8136589760
Vladkolo <Vladkolo2014@gmail.com>
2021-01-12 00:41:58 +0200

    error

---
+++ ChiHuashki/LendingApp/admin.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm
+from django.contrib import admin
+from .models import Chihuahua, Photo
+from django.utils.html import mark_safe
+
+# admin.site.register(Chihuahua)
+
+
+class PhotoForm(ModelForm):
+    MIN_RESOLUTION = (100, 200)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загрузите изображение с минимальным разрешением {}x{}'.format(
+            *self.MIN_RESOLUTION)
+
+
+class GalleryInline(admin.TabularInline):
+    fk_name = 'chihuahua'
+    model = Photo
+    form = PhotoForm
+    extra = 1  # количество пустых форм
+    max_num = 5  # максимальное количество форм
+    verbose_name = 'image'
+
+
+
+@admin.register(Chihuahua)
+class ChihuahuaAdmin(admin.ModelAdmin):
+    inlines = [GalleryInline, ]
+    list_display = ("name", "age", "teeth")
+
+
