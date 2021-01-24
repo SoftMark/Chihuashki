59973697052dc49bff340600e9c421762e21e403
Vladkolo <Vladkolo2014@gmail.com>
2021-01-25 01:31:30 +0200

    slide fodo and gallery

--- ChiHuashki/static/js/script.js
+++ ChiHuashki/static/js/script.js
@@ -1,60 +1,92 @@
-let butRise = document.getElementById("topChi");
-let contents = document.getElementsByClassName("content");
-let count = 0;
-let width;
+let butRise = document.getElementById("topChi"),
+    arrowPrevious = document.getElementById("previous-img"),
+    arrowNext = document.getElementById("next-img"),
+    contents = document.getElementsByClassName("content"),
+    imagesDog = document.querySelectorAll(".images"),
+    count = 0,
+    countGallery = 0,
+    width;
 
-
-// document.querySelectorAll('.images').forEach(dog => {
-//     dog.addEventListener("mouseover", function (e) {
+// Scroll slow
+// document.querySelectorAll('a[href^="#"').forEach(link => {
+//     link.addEventListener("click", function (e) {
 //         e.preventDefault();
 //
-//         init(this);
+//         let href = this.getAttribute("href").substring(1);
+//         const scrollTarget = document.getElementById(href);
+//         const topOffset = document.querySelector("#menu-links").offsetHeight;
+//         const elementPosition = scrollTarget.getBoundingClientRect().top;
+//         const offsetPosition = elementPosition - topOffset;
 //
+//         window.scrollBy({
+//             top: offsetPosition,
+//             behavior: "smooth"
+//         });
 //     });
 // });
 
-function init(photo){
-    let dog_line = photo.querySelector('.img-line');
-    let dog_images = photo.querySelectorAll('.img-dog');
-    console.log("resize");
-    width = photo.offsetWidth;
-    if (dog_line)
-        dog_line.style.width = width * dog_images.length + "px";
-    console.log(width);
-    console.log(dog_line);
-    console.log(dog_images);
-}
+// Slide images dog
+imagesDog.forEach(img => {
+    let count = 0;
+    img.querySelectorAll(".arrow-dog").forEach( arrow => {
 
-document.querySelectorAll('a[href^="#"').forEach(link => {
-    link.addEventListener("click", function (e) {
-        e.preventDefault();
+        arrow.addEventListener("click", function (e) {
+            e.preventDefault();
 
-        let href = this.getAttribute("href").substring(1);
-        const scrollTarget = document.getElementById(href);
-        const topOffset = document.querySelector("#menu-links").offsetHeight;
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
+            let images = img.querySelector(".img-line"),
+                imgCol = images.querySelectorAll("img").length,
+                imgWidth = 20;
 
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: "smooth"
+
+            if (this.classList[1] == "previous-img") {
+                count--;
+                if (count < 0){
+                    count = imgCol - 1;
+                }
+            } else {
+                count++;
+                if (count > imgCol - 1){
+                    count = 0;
+                }
+            }
+            images.style.transform = "translate(-"+count*imgWidth+"vw)";
         });
     });
 });
 
-// document.querySelectorAll(".title").forEach(id => {
-//     id.addEventListener("scroll", function (e) {
-//         e.preventDefault();
-//
-//
-//     });
-// });
+
+// Slide gallery dog
+document.querySelectorAll(".gallery-arrow").forEach( arrow => {
+    arrow.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let images = document.querySelector(".gallery-list"),
+            imgCol = images.querySelectorAll(".block-img").length,
+            imgWidth = 22.7;
+
+
+        if (this.classList[1] == "gallery-previous") {
+            countGallery--;
+            if (countGallery < 0){
+                countGallery = imgCol - 1;
+            }
+        } else {
+            countGallery++;
+            if (countGallery > imgCol - 1){
+                countGallery = 0;
+            }
+        }
+        console.log(countGallery);
+        images.style.transform = "translate(-"+countGallery*imgWidth+"vw)";
+    });
+});
 
 window.addEventListener("scroll", function() {
+    console.log("scroll")
     const value = window.scrollY;
 
     if (value >= 300) {
-        butRise.removeAttribute("style");
+        butRise.style.display = "block";
     } else if (value === 0){
         butRise.style.display = "none";
     }
